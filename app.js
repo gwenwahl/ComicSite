5d376c97b0dd9500d1c28ac3f5f41812ff37d2cf
Gwendolyn Wahl <wahl.gwen@gmail.com>
2015-01-17 05:58:56 +0000

    adding first portions of site

---
+++ app.js
@@ -0,0 +1,35 @@
+/**
+ * Created by Gwendolyn on 1/16/15.
+ */
+
+/*
+ * Module dependencies
+ */
+var   express = require('express')
+    , stylus  = require('stylus')
+    , nib     = require('nib');
+
+var app = express();
+function compile(str, path) {
+    return stylus(str)
+        .set('filename', path)
+        .use(nib())
+}
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(stylus.middleware(
+    { src: __dirname + '/public'
+        , compile: compile
+    }
+));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+    res.render('index',
+        { title : 'Home' }
+    );
+});
+
+app.listen(80, function () {
+    console.log('Application listening on port 8080');
+});
